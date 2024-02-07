686c73cca4563d273206198cf6e077ed8616ddda
Lean-b <pentestinglean@gmail.com>
2024-02-07 00:03:27 -0300

    solidity

---
+++ test/Transactions.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {Transactions} from "../src/Transactions.sol";
+
+contract transactionsTest is Test {
+    Transactions public transactions;
+
+    function setUp() public {
+        transactions = new Transactions();
+
+    }
+
+
+}
