13f9e761fe22c6dfee2e4255f7786c19de49d15c
Lean-b <pentestinglean@gmail.com>
2024-02-17 17:58:53 -0300

    Comment

--- src/Transactions.sol
+++ src/Transactions.sol
@@ -3,11 +3,29 @@ pragma solidity ^0.8.13;
 
 import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract transactions is Ownable(msg.sender) {
-    error transactionError(string Error);
+/*//////////////////////////////////////////////////////////////
+                Transactions Solidity Ethereum 
+//////////////////////////////////////////////////////////////*/
 
+/**
+ * @author  Leandro.
+ * @title   Transactions in Blockachain.
+ * @notice  Practice project.
+ */
+contract transactions is Ownable{
+
+    /// Count the quantity of transaction.
     uint private transactionCount;
+    uint16 public amountEther;
+    
 
+
+    constructor(address _proprietor, uint16 _amountEther) Ownable(_proprietor){
+        amountEther = _amountEther;
+    }
+
+
+    /// Register structure of each transactions
     struct Register {
         address sender;
         address receiver;
@@ -17,6 +35,7 @@ contract transactions is Ownable(msg.sender) {
 
     Register[] register;
 
+    /// Event log transaction between tow addresses with amount and menssage.
     event Transaction(
         address indexed from,
         address indexed receiver,
@@ -24,23 +43,46 @@ contract transactions is Ownable(msg.sender) {
         string messager
     );
 
+
+    /**
+     * @notice  .
+     * @dev     .
+     * @param   _receiver  .
+     * @param   _amount  .
+     * @param   _messager  .
+     */
     function addRegistred(
         address _receiver,
         uint _amount,
         string memory _messager
     ) public {
         transactionCount += 1;
+
         register.push(Register(msg.sender, _receiver, _amount, _messager));
+
+        require(_amount >= amountEther, "Insufficient amount of ether");
+
         emit Transaction(msg.sender, _receiver, _amount, _messager);
+
     }
 
-    function getRegistre(
-        uint8 _index
-    ) public view returns (Register memory) {
+    /**
+     * @notice  .
+     * @dev     .
+     * @param   _index  .
+     * @return  Register  .
+     */
+    function getRegistre(uint8 _index) public view returns (Register memory) {
         require(_index < register.length, "Invalid index");
         return register[_index];
     }
 
+    /**
+     * @notice  .
+     * @dev     .
+     * @param   _index  .
+     * @return  Register[]  .
+     */
     function getRegistreAll(
         uint8 _index
     ) public view returns (Register[] memory) {
@@ -48,6 +90,11 @@ contract transactions is Ownable(msg.sender) {
         return register;
     }
 
+    /**
+     * @notice  .
+     * @dev     .
+     * @param   _index  .
+     */
     function deleteArrayElement(uint8 _index) public onlyOwner {
         require(_index < register.length, "Invalid index");
 
@@ -57,6 +104,11 @@ contract transactions is Ownable(msg.sender) {
         register.pop();
     }
 
+    /**
+     * @notice  .
+     * @dev     .
+     * @param   _index  .
+     */
     function deleteArrayElementAll(uint8 _index) public onlyOwner {
         require(_index < register.length, "Invalid index");
         delete register;
